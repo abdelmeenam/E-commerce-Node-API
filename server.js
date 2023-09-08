29fc10a4e5e1946e9aa966c8deaed6f6dcb9ba4e
abdelmeenam <abdelmeenam@gmail.com>
2023-09-08 15:42:19 +0300

    Error handling and Validation layer

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 
 dotenv.config({ path: 'config.env' })
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/db');
 const categoryRoute = require('./routes/categoryRoute');
 
-// DB  , app
+
+// DB , app
 dbConnection();
 const app = express()
 
@@ -17,7 +20,18 @@ if (process.env.NODE_ENV === 'development') { app.use(morgan('dev')) }
 
 // routes
 app.use('/api/v1/categories', categoryRoute);
+app.all('*', (req, res, next) => {
+    // catch error and send to error handeling middleware
+    //const err = new Error(`Page not found : ${req.originalUrl}`);
+    //next(err.message);
 
+    // or use ApiError class
+    next(new ApiError(`Page not found : ${req.originalUrl}`, 400))
+})
+
+
+// Global error handeling middleware provided by express
+app.use(globalError)
 
 
 const port = process.env.PORT || 8000;
