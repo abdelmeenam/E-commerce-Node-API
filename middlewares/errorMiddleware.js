8b186b09e568d91b3c656f7c8feb18b47d896a27
abdelmeenam <abdelmeenam@gmail.com>
2023-09-09 23:44:36 +0300

    Error handling and Validation layer

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,14 +1,23 @@
 const globalError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
+    err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    }
+
+
+}
+
+const sendErrorDev = (err, res) => {
+    return res.status(err.statusCode).json({
         status: err.status,
         error: err,
         message: err.message,
         stack: err.stack
-    });
-
+    })
 }
+
+
 
 module.exports = globalError;
