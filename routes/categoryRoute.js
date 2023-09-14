2817892f6a238ff85a74d484c0d9358c4036edba
abdelmeenam <abdelmeenam@gmail.com>
2023-09-14 17:29:41 +0300

    Error handling and Validation layer

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const router = express.Router();
+const { param, validationResult } = require('express-validator');
+
 const { getCategories, createCategory, getCategory, deleteCategory, updateCategory } = require('../services/categoryService');
 
-
+const router = express.Router();
 router.route('/').get(getCategories).post(createCategory)
-router.route('/:id').get(getCategory).delete(deleteCategory).put(updateCategory)
+router.route('/:id')
+    .get(param('id').isMongoId().withMessage("Invalid category id"),
+        (req, res) => {
+            const result = validationResult(req);
+            if (!result.isEmpty()) {
+                return res.status(400).json({ errors: result.array() });
+            }
+        }
+        , getCategory)
+    .delete(deleteCategory)
+    .put(updateCategory)
 
 
 module.exports = router;
